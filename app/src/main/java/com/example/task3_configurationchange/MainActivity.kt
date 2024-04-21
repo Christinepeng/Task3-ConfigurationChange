1200d30817fedb7baa0135f295328ac2b864c89c
Yellow Bear <percussionpeng@gmail.com>
2024-04-21 15:20:16 -0700

    finish

--- app/src/main/java/com/example/task3_configurationchange/MainActivity.kt
+++ app/src/main/java/com/example/task3_configurationchange/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.task3_configurationchange
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private companion object {
+        const val KEY_COUNTER = "counter"
+        const val MAX_COUNTER_VALUE = 1000
+    }
+
+    private var counter = 0
+    private lateinit var counterTextView: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +28,29 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        counterTextView = findViewById(R.id.textView)
+
+        if (savedInstanceState != null) {
+            counter = savedInstanceState.getInt(KEY_COUNTER)
+            updateCounterText()
+        }
+
+        val button: Button = findViewById(R.id.button)
+        button.setOnClickListener {
+            if (counter < MAX_COUNTER_VALUE) {
+                counter++
+                updateCounterText()
+            }
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt(KEY_COUNTER, counter)
+        super.onSaveInstanceState(outState)
+    }
+
+    private fun updateCounterText() {
+        counterTextView.text = counter.toString()
     }
 }
